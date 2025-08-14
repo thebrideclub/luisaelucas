e6d7e32d9be78b99110316030d10adc3364bcd3b
thebrideclub <thebrideclubb@gmail.com>
2025-08-14 00:55:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,12 @@ function atualizarContagem() {
 
 atualizarContagem();
 setInterval(atualizarContagem, 1000);
+
+window.addEventListener("scroll", function () {
+  const menu = document.querySelector(".menu");
+  if (window.scrollY > 50) {
+    menu.classList.add("scrolled");
+  } else {
+    menu.classList.remove("scrolled");
+  }
+});
